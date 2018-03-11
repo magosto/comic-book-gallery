880350817e7dc00383d37de85d3b2af8e79e0fd4
Matt <matthewragosto@robotsandpencils.com>
2018-03-11 14:28:26 -0400

    Updated commic book detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,18 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue!</p>";
+            ViewBag.Artists = new string[]
+            {
+              "Script: Dan Slott",
+               "Pencil: Humberto Ramos",
+              "Inks: Victor Olazaba"
+            };
+
+
             return View();
         }
     }
